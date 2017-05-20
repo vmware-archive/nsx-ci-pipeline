a11815ffe3ee46b99225f4188bd2d5243d492c92
Sabha Parameswaran <sabhap@pivotal.io>
2017-05-19 17:55:05 -0700

    reuse nsx-gen list

--- tasks/nsx-edge-destroy/task.sh
+++ tasks/nsx-edge-destroy/task.sh
@@ -7,14 +7,26 @@ pushd nsx-edge-gen
 # Remove any existing config file template from the repo
 if [[ -e nsx_cloud_config.yml ]]; then rm -rf nsx_cloud_config.yml; fi
 
-
 # Init new config file template
 ./nsx-gen/bin/nsxgen -i $NSX_EDGE_GEN_NAME init
+if [[ ISOZONE_SWITCH_NAME_1 ]]; then
+  ARGS="
+  -isozone_switch_name_1 $ISOZONE_SWITCH_NAME_1
+  -isozone_switch_cidr_1 $ISOZONE_SWITCH_CIDR_1
+  -esg_go_router_isozone_1_uplink_ip_1 $ESG_GO_ROUTER_ISOZONE_1_UPLINK_IP_1
+  -esg_go_router_isozone_1_switch_1 $ISOZONE_SWITCH_NAME_1
+  -esg_go_router_isozone_1_inst_1 $ESG_GO_ROUTER_ISOZONE_1_INST_1
+  -esg_tcp_router_isozone_1_uplink_ip_1 $ESG_TCP_ROUTER_ISOZONE_1_UPLINK_IP_1 
+  -esg_tcp_router_isozone_1_switch_1 $ISOZONE_SWITCH_NAME_1
+  -esg_tcp_router_isozone_1_inst_1 $ESG_TCP_ROUTER_ISOZONE_1_INST_1
+  "
+fi
 
 ./nsx-gen/bin/nsxgen \
 -c $NSX_EDGE_GEN_NAME \
 -esg_name_1 $NSX_EDGE_GEN_NAME \
 -esg_size_1 $ESG_SIZE  \
+-esg_ospf_password_1 $ESG_OSPF_PASSWORD_1  \
 -esg_cli_user_1 $ESG_CLI_USERNAME_1   \
 -esg_cli_pass_1 $ESG_CLI_PASSWORD_1   \
 -esg_certs_1 $ESG_CERTS_NAME_1   \
@@ -35,8 +47,10 @@ if [[ -e nsx_cloud_config.yml ]]; then rm -rf nsx_cloud_config.yml; fi
 -nsxmanager_user $NSX_EDGE_GEN_NSX_MANAGER_ADMIN_USER   \
 -nsxmanager_pass $NSX_EDGE_GEN_NSX_MANAGER_ADMIN_PASSWD   \
 -nsxmanager_tz $NSX_EDGE_GEN_NSX_MANAGER_TRANSPORT_ZONE   \
+-nsxmanager_dportgroup $NSX_EDGE_GEN_NSX_MANAGER_DISTRIBUTED_PORTGROUP \
 -nsxmanager_uplink_ip $ESG_DEFAULT_UPLINK_IP_1  \
 -nsxmanager_uplink_port "$ESG_DEFAULT_UPLINK_PG_1" \
+$ARGS \
 delete
 
 
