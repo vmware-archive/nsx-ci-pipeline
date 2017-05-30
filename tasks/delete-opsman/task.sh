db2805d97736521549e82de7107a1b14891a14fe
Sabha Parameswaran <sabhap@pivotal.io>
2017-05-30 16:37:34 -0700

    use specified config for mysql/rabbit proxy instances

--- tasks/delete-opsman/task.sh
+++ tasks/delete-opsman/task.sh
@@ -4,5 +4,9 @@ gunzip ./govc/govc_linux_amd64.gz
 chmod +x ./govc/govc_linux_amd64
 
 export GOVC_IPATH="/Datacenter/vm/$OM_VM_NAME"
+export GOVC_INSECURE=1
+export GOVC_URL=$GOVC_URL
+export GOVC_USERNAME=$GOVC_USERNAME
+export GOVC_PASSWORD=$GOVC_PASSWORD
 
 ./govc/govc_linux_amd64 vm.destroy -vm.ipath=$GOVC_IPATH
