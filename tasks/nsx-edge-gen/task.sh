716acffa83a3f9226205f44c836df25a8a4785d0
Sabha Parameswaran <sabhap@pivotal.io>
2017-08-10 14:51:56 -0400

    wrap params in quotes

--- tasks/nsx-edge-gen/task.sh
+++ tasks/nsx-edge-gen/task.sh
@@ -83,15 +83,14 @@ fi
 
 
 ./nsx-gen/bin/nsxgen \
--c $NSX_EDGE_GEN_NAME \
--esg_name_1 $NSX_EDGE_GEN_NAME \
--esg_size_1 $ESG_SIZE  \
--esg_ospf_password_1 $ESG_OSPF_PASSWORD_1  \
--esg_cli_user_1 $ESG_CLI_USERNAME_1   \
--esg_cli_pass_1 $ESG_CLI_PASSWORD_1   \
--esg_ert_certs_1 $ESG_ERT_CERTS_NAME_1   \
--esg_ert_certs_config_sysd_1 $ESG_ERT_CERTS_CONFIG_SYSTEMDOMAIN_1   \
--esg_ert_certs_config_appd_1 $ESG_ERT_CERTS_CONFIG_APPDOMAIN_1   \
+-c "$NSX_EDGE_GEN_NAME" \
+-esg_name_1 "$NSX_EDGE_GEN_NAME" \
+-esg_ospf_password_1 "$ESG_OSPF_PASSWORD_1"  \
+-esg_cli_user_1 "$ESG_CLI_USERNAME_1"   \
+-esg_cli_pass_1 "$ESG_CLI_PASSWORD_1"   \
+-esg_ert_certs_1 "$ESG_ERT_CERTS_NAME_1"   \
+-esg_ert_certs_config_sysd_1 "$ESG_ERT_CERTS_CONFIG_SYSTEMDOMAIN_1"   \
+-esg_ert_certs_config_appd_1 "$ESG_ERT_CERTS_CONFIG_APPDOMAIN_1"   \
 -esg_opsmgr_uplink_ip_1 $ESG_OPSMGR_UPLINK_IP_1   \
 -esg_go_router_uplink_ip_1 $ESG_GO_ROUTER_UPLINK_IP_1   \
 -esg_diego_brain_uplink_ip_1 $ESG_DIEGO_BRAIN_UPLINK_IP_1   \
@@ -103,15 +102,15 @@ fi
 -esg_mysql_tile_inst_1 $ESG_MYSQL_TILE_PROXY_INSTANCES_1  \
 -esg_rabbitmq_tile_inst_1 $ESG_RABBITMQ_TILE_PROXY_INSTANCES_1 \
 -esg_gateway_1 $ESG_GATEWAY_1 \
--vcenter_addr $VCENTER_HOST   \
--vcenter_user $VCENTER_USR   \
--vcenter_pass $VCENTER_PWD   \
--vcenter_dc $VCENTER_DATA_CENTER   \
--vcenter_ds $NSX_EDGE_GEN_EDGE_DATASTORE   \
--vcenter_cluster $NSX_EDGE_GEN_EDGE_CLUSTER  \
--nsxmanager_addr $NSX_EDGE_GEN_NSX_MANAGER_ADDRESS   \
--nsxmanager_user $NSX_EDGE_GEN_NSX_MANAGER_ADMIN_USER   \
--nsxmanager_pass $NSX_EDGE_GEN_NSX_MANAGER_ADMIN_PASSWD   \
+-vcenter_addr "$VCENTER_HOST"   \
+-vcenter_user "$VCENTER_USR"   \
+-vcenter_pass "$VCENTER_PWD"   \
+-vcenter_dc "$VCENTER_DATA_CENTER"   \
+-vcenter_ds "$NSX_EDGE_GEN_EDGE_DATASTORE"   \
+-vcenter_cluster "$NSX_EDGE_GEN_EDGE_CLUSTER"  \
+-nsxmanager_addr "$NSX_EDGE_GEN_NSX_MANAGER_ADDRESS"  \
+-nsxmanager_user "$NSX_EDGE_GEN_NSX_MANAGER_ADMIN_USER"   \
+-nsxmanager_pass "$NSX_EDGE_GEN_NSX_MANAGER_ADMIN_PASSWD"   \
 -nsxmanager_tz "$NSX_EDGE_GEN_NSX_MANAGER_TRANSPORT_ZONE"   \
 -nsxmanager_tz_clusters "$NSX_EDGE_GEN_NSX_MANAGER_TRANSPORT_ZONE_CLUSTERS" \
 -nsxmanager_dportgroup "$NSX_EDGE_GEN_NSX_MANAGER_DISTRIBUTED_PORTGROUP" \
--- tasks/nsx-edge-list/task.sh
+++ tasks/nsx-edge-list/task.sh
@@ -91,14 +91,14 @@ if [ "$ISOZONE_SWITCH_NAME_3" != "" ]; then
 fi
 
 ./nsx-gen/bin/nsxgen \
--c $NSX_EDGE_GEN_NAME \
--esg_name_1 $NSX_EDGE_GEN_NAME \
--esg_ospf_password_1 $ESG_OSPF_PASSWORD_1  \
--esg_cli_user_1 $ESG_CLI_USERNAME_1   \
--esg_cli_pass_1 $ESG_CLI_PASSWORD_1   \
--esg_ert_certs_1 $ESG_ERT_CERTS_NAME_1   \
--esg_ert_certs_config_sysd_1 $ESG_ERT_CERTS_CONFIG_SYSTEMDOMAIN_1   \
--esg_ert_certs_config_appd_1 $ESG_ERT_CERTS_CONFIG_APPDOMAIN_1   \
+-c "$NSX_EDGE_GEN_NAME" \
+-esg_name_1 "$NSX_EDGE_GEN_NAME" \
+-esg_ospf_password_1 "$ESG_OSPF_PASSWORD_1"  \
+-esg_cli_user_1 "$ESG_CLI_USERNAME_1"   \
+-esg_cli_pass_1 "$ESG_CLI_PASSWORD_1"   \
+-esg_ert_certs_1 "$ESG_ERT_CERTS_NAME_1"   \
+-esg_ert_certs_config_sysd_1 "$ESG_ERT_CERTS_CONFIG_SYSTEMDOMAIN_1"   \
+-esg_ert_certs_config_appd_1 "$ESG_ERT_CERTS_CONFIG_APPDOMAIN_1"   \
 -esg_opsmgr_uplink_ip_1 $ESG_OPSMGR_UPLINK_IP_1   \
 -esg_go_router_uplink_ip_1 $ESG_GO_ROUTER_UPLINK_IP_1   \
 -esg_diego_brain_uplink_ip_1 $ESG_DIEGO_BRAIN_UPLINK_IP_1   \
@@ -110,18 +110,18 @@ fi
 -esg_mysql_tile_inst_1 $ESG_MYSQL_TILE_PROXY_INSTANCES_1  \
 -esg_rabbitmq_tile_inst_1 $ESG_RABBITMQ_TILE_PROXY_INSTANCES_1 \
 -esg_gateway_1 $ESG_GATEWAY_1 \
--vcenter_addr $VCENTER_HOST   \
--vcenter_user $VCENTER_USR   \
--vcenter_pass $VCENTER_PWD   \
--vcenter_dc $VCENTER_DATA_CENTER   \
--vcenter_ds $NSX_EDGE_GEN_EDGE_DATASTORE   \
--vcenter_cluster $NSX_EDGE_GEN_EDGE_CLUSTER  \
--nsxmanager_addr $NSX_EDGE_GEN_NSX_MANAGER_ADDRESS   \
--nsxmanager_user $NSX_EDGE_GEN_NSX_MANAGER_ADMIN_USER   \
--nsxmanager_pass $NSX_EDGE_GEN_NSX_MANAGER_ADMIN_PASSWD   \
+-vcenter_addr "$VCENTER_HOST"   \
+-vcenter_user "$VCENTER_USR"   \
+-vcenter_pass "$VCENTER_PWD"   \
+-vcenter_dc "$VCENTER_DATA_CENTER"   \
+-vcenter_ds "$NSX_EDGE_GEN_EDGE_DATASTORE"   \
+-vcenter_cluster "$NSX_EDGE_GEN_EDGE_CLUSTER"  \
+-nsxmanager_addr "$NSX_EDGE_GEN_NSX_MANAGER_ADDRESS"  \
+-nsxmanager_user "$NSX_EDGE_GEN_NSX_MANAGER_ADMIN_USER"   \
+-nsxmanager_pass "$NSX_EDGE_GEN_NSX_MANAGER_ADMIN_PASSWD"   \
 -nsxmanager_tz "$NSX_EDGE_GEN_NSX_MANAGER_TRANSPORT_ZONE"   \
 -nsxmanager_tz_clusters "$NSX_EDGE_GEN_NSX_MANAGER_TRANSPORT_ZONE_CLUSTERS" \
--nsxmanager_dportgroup $NSX_EDGE_GEN_NSX_MANAGER_DISTRIBUTED_PORTGROUP \
+-nsxmanager_dportgroup "$NSX_EDGE_GEN_NSX_MANAGER_DISTRIBUTED_PORTGROUP" \
 -nsxmanager_uplink_ip $ESG_DEFAULT_UPLINK_IP_1  \
 -nsxmanager_uplink_port "$ESG_DEFAULT_UPLINK_PG_1" \
 -nsxmanager_en_dlr $NSX_EDGE_GEN_ENABLE_DLR \
