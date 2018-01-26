9be5f149f270cec2e193cb7a9207b71e45d7984e
Sabha Parameswaran <sabhap@pivotal.io>
2018-01-25 17:54:02 -0800

    support pas 2.0

--- tasks/delete-opsman/task.sh
+++ tasks/delete-opsman/task.sh
@@ -13,16 +13,18 @@ echo "Done finding possible_opsmans : $possible_opsmans"
 
 # REMOVE ME - temporarily to clean up ops mgr
 if [ "$possible_opsmans" == "" ]; then
-  possible_opsmans=$(govc find $resource_pool_path -type m -name ${OPS_NAME} )
+  possible_opsmans=$(govc find $resource_pool_path -type m -name ${OM_VM_NAME} )
 fi
 
 for opsman in ${possible_opsmans}; do
+  echo "ops man is $opsman"
   network="$(govc vm.info -r=true -json ${opsman} | jq -r '.VirtualMachines[0].Guest.Net[0].Network')"
-  if [[ ${network} == ${GOVC_NETWORK} || ${network} == "" ]]; then
+  echo "network is is $network"
+  if [[ ${network} == ${GOVC_NETWORK} ]]; then
     echo "Powering off and removing ${opsman}..."
     set +e
     govc vm.power -vm.ipath=${opsman} -off
-    set -e
-    govc vm.destroy -vm.ipath=${opsman}
+    set -e    
   fi
+  govc vm.destroy -vm.ipath=${opsman}
 done
