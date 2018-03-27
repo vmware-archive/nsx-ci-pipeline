ab76f17cbba94d7bf8e25700bdebdfc034c39e57
Sabha Parameswaran <sabhap@pivotal.io>
2018-03-26 18:05:18 -0700

    Allow configuration of grootfs

--- tasks/config-ert-2.0/task.sh
+++ tasks/config-ert-2.0/task.sh
@@ -155,7 +155,7 @@ if [ "$CREDHUB_PASSWORD" == "" ]; then
 fi
 
 has_blobstore_internal_access_subnet=$(echo $STAGED_PRODUCT_PROPERTIES | jq . | grep ".nfs_server\.blobstore_internal_access_rules" | wc -l || true)
-
+has_grootfs=$(echo $STAGED_PRODUCT_PROPERTIES | jq . | grep ".properties\.enable_grootfs" | wc -l || true)
 
 cf_properties=$(
   jq -n \
@@ -231,6 +231,8 @@ cf_properties=$(
     --arg container_networking_interface_plugin "$CONTAINER_NETWORKING_INTERFACE_PLUGIN" \
     --arg has_blobstore_internal_access_subnet "$has_blobstore_internal_access_subnet" \
     --arg blobstore_internal_access_subnet "$BLOBSTORE_INTERNAL_ACCESS_SUBNET" \
+    --arg has_grootfs "$has_grootfs" \
+    --arg enable_grootfs "${ENABLE_GROOTFS:true}" \
     '
     {
       ".properties.system_blobstore": {
@@ -303,6 +305,19 @@ cf_properties=$(
     {
         ".nfs_server.blobstore_internal_access_rules": {
         "value": $blobstore_internal_access_subnet
+      }
+    }
+    else
+    .
+    end
+
+    +
+
+    # Grootfs option
+    if $has_grootfs != "0" then
+    {
+        ".properties.enable_grootfs": {
+        "value": $ENABLE_GROOTFS
       }
     }
     else
